0963c66a97136ac2d4b1868373c3d0fcadd5daa7
Pavel Pavel <frainvoc@gmail.com>
2025-07-25 18:44:18 +0200

    Initial commit: Xcode Project Init

---
+++ Slip-Fi/Slip_FiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slip_FiApp.swift
+//  Slip-Fi
+//
+//  Created by Pavel Pavel on 25/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct Slip_FiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
